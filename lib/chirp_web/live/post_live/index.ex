dae0182906e872fdc97a868f463180a79e8ccf3d
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-04-06 10:09:44 +0100

    New post event transmission

--- lib/chirp_web/live/post_live/index.ex
+++ lib/chirp_web/live/post_live/index.ex
@@ -6,7 +6,8 @@ defmodule ChirpWeb.PostLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :posts, list_posts())}
+    if connected?(socket), do: Timeline.subscribe()
+    {:ok, assign(socket, :posts, fetch_posts())}
   end
 
   @impl true
@@ -37,10 +38,15 @@ defmodule ChirpWeb.PostLive.Index do
     post = Timeline.get_post!(id)
     {:ok, _} = Timeline.delete_post(post)
 
-    {:noreply, assign(socket, :posts, list_posts())}
+    {:noreply, assign(socket, :posts, fetch_posts())}
   end
 
-  defp list_posts do
+  @impl true
+  def handle_info({:post_created, post}, socket) do
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
+  end
+
+  defp fetch_posts do
     Timeline.list_posts()
   end
 end
